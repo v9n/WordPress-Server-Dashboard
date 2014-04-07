9a9cda3c1e4c39a7030d68c658dcdb5fbf335d3f
kureikain <kureikain@gmail.com>
2014-04-06 19:01:08 -0700

    new arch

--- Server Dashboard.php
+++ Server Dashboard.php
@@ -13,46 +13,57 @@ namespace AX\StatBoard;
 
 use AX\StatBoard\Widget;
 
-require_once plugin_dir_path( __FILE__ ) . '/widget.php' ;
-require_once plugin_dir_path( __FILE__ ) . '/widget/provider.php' ;
-
 class Dashboard {
   protected static $_instance=NULL;
   protected $_dashboard_widget = array();
+  protected $_plugin_dir=NULL;
 
+  /**
+   * Auto load class under namespace of this plugin
+   */
+  public function load_class($classname)
+  {
+    if (FALSE === $pos = strpos($classname, 'AX\StatBoard')) {
+      return false;
+    }
+    $classname = substr($classname, $pos+1 + strlen('AX\StatBoard'));
+    $filepath = $this->_plugin_dir . strtolower(str_replace('\\', '/', $classname) . '.php');
+    if (!file_exists($filepath)) {
+      return false;
+    }
+    include $filepath;
+  } 
+  
+  /**
+   * Setup variable and intialize widget provider
+   */
   function __construct() {
+    $this->_plugin_dir =  plugin_dir_path( __FILE__ ) ;
+    spl_autoload_register(array($this, 'load_class'));
+
     $this->_dashboard_widget = array(
       'server',
-      //'hello',
-      'disk',
+      //'cpu_load',
+
       'ram',
-  
-  //const DASHBOARD_WIDGET_SERVER_LOAD    = 20;
+      
+      'disk',
+      'diskio',
 
-  //const DASHBOARD_WIDGET_MEMORY         = 30;
-
-  //const DASHBOARD_WIDGET_DISK_IO        = 41;
-  
-  //const DASHBOARD_WIDGET_SOFTWARE       = 50;
-  
-  //const DASHBOARD_WIDGET_SERVER_IP      = 60;
-
-  //const DASHBOARD_WIDGET_INTERNET_SPEED = 70;
-  //const DASHBOARD_WIDGET_NETWORK_IO     = 71;
-  
-  //const DASHBOARD_WIDGET_PROCESS        = 80;
+      'software',
+      'ethernet',
+      
+      'internetspeed',
+      'networkio',
+      'process',
     );
+
     foreach ($this->_dashboard_widget as $item) {
-      if (!file_exists(plugin_dir_path( __FILE__ ) . '/widget/' . $item . '.php')) {
+      if (!file_exists($this->_plugin_dir . '/widget/' . $item . '.php')) {
         continue;
       }
-      require_once plugin_dir_path( __FILE__ ) . '/widget/' . $item . '.php' ;
       $classname = 'AX\\StatBoard\\Widget\\' . ucwords($item);
-      //$class = new \ReflectionClass('ReflectionClass');
-      //$p = $class->newInstance($classname);
-      $p = new $classname();
-      Widget::instance()->add_provider($item, $p);
-      //Widget::instance()->add_provider($item, new Hello());
+      Widget::instance()->add_provider($item, new $classname());
     }
   }
 
@@ -69,15 +80,13 @@ class Dashboard {
   public function run() {
     add_action( 'wp_dashboard_setup', array( $this, 'add_dashboard_widgets' ) );
     add_action( 'admin_enqueue_scripts', array($this, 'add_asset'));
-    add_action('wp_footer', array($this, 'footer'));
+    add_action( 'wp_footer', array($this, 'footer'));
   }
-
-  function remove_dashboard_widgets() {
-
-  }
-
+  
+  /**
+   * Register dashboard widget proider to show up on dashboard
+   */
   function add_dashboard_widgets() {
-    syslog(LOG_DEBUG, "Run"); 
     $widget = Widget::instance();
     foreach ($widget->get_provider() as $name=>$provider) {
       $widget->register($name);
@@ -85,19 +94,21 @@ class Dashboard {
   }
 
   /**
-   * Add java script
+   * Assets load: stylesheet, JS. 
    */ 
   function add_asset() {
-    //wp_enqueue_style( 'style-name', get_stylesheet_uri() );
     syslog(LOG_DEBUG, "Loaded"); 
     wp_enqueue_script( 'google-chart', 'https://www.google.com/jsapi' );
-    wp_enqueue_script( 'plugin_dir_url', plugin_dir_url(__FILE__) . '/loader.js');
+    //wp_enqueue_script( 'plugin_dir_url', plugin_dir_url(__FILE__) . '/loader.js');
   }
 
+  /**
+   * Inline JavaScript for chart
+   */
   function footer() {
     echo '
       <script>google.load("visualization", "1", {packages:["corechart"]})</script>
-      ';
+';
   }
 }
 
